28cb41f4804cac68831c9df3043df2cc2b552efc
Logesh <logeshc18022002@gmail.com>
2024-11-28 17:06:33 +0530

    refactor

--- src/components/ListPerson.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Person = ({person}) => {
-  return (
-    <h6>I am {person.name} and my age is {person.age} learing {person.skill} {person.id}</h6>
-  )
-}
-
-export default Person
